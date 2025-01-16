cf70fc05678c76e70c2c360b8a03f2d1dc09a27a
Shi Qiu <qsly09@hotmail.com>
2025-01-16 16:34:44 -0500

    Update autoFmaskBatch.m

--- autoFmaskBatch.m
+++ autoFmaskBatch.m
@@ -2,7 +2,7 @@ function autoFmaskBatch()
 % This can automatically find all Landsats 4-8 and Sentinel-2 images
 % (folder) and to process them one by one.
 
-    fprintf('Fmask 4.6 batch starts ...\n');
+    fprintf('Fmask 4.7 batch starts ...\n');
     filepath_work = pwd; % CD to the path; or, type in the path of your working directory here.
     [~, ~, paths, ~] = CheckImagesPath(filepath_work);
     for i=1: length(paths)
